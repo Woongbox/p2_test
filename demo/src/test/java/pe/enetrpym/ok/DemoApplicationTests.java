87ab460776d56aea8a9dbfa4026d78fd92dc37d9
cw.park <cw.park@kohyoung.com>
2020-04-27 21:55:24 +0900

    init commit

---
+++ demo/src/test/java/pe/enetrpym/ok/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.enetrpym.ok;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
